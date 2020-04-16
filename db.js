5deefc6d09c4f5f988a6269f86152c3c9ac44330
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-04-16 13:40:27 +0100

    Import dotenv
    Loads environment variables from .env file to process.env

--- db.js
+++ db.js
@@ -1,19 +1,25 @@
 //import mongoose library
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-//MONGO_URI 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/url'; 
+dotenv.config();
+
+//MONGO_URI
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/url';
+
+console.log(process.env.MONGO_URI);
 
 //Connect to DB function
 const connect = () => {
-  mongoose.connect(MONGO_URI, {useNewUrlParser : true, useUnifiedTopology : true})
-  .then(() => console.log('DB connected'))
-  .catch(err => console.log(err));
+  mongoose
+    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    .catch((err) => console.log(err));
   //On connection error, log the message
-  mongoose.connection.on('error', err => {
+  mongoose.connection.on('error', (err) => {
     console.log(`DB connection error : ${err.message}`);
   });
-}
+};
 
 //export the connect function, to use in server.js
-module.exports = { connect }; 
+module.exports = { connect };
